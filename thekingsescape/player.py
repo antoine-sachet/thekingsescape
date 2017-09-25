9fc4888be891d86f6a1c82ca5d8faff73a1cfe0f
Antoine Sachet <antoine.sac@gmail.com>
2017-09-25 20:20:50 +0100

    Added repr method to Player class

--- thekingsescape/player.py
+++ thekingsescape/player.py
@@ -2,5 +2,8 @@ class Player:
     def __init__(self, name):
         self.name = name
 
-    def perform(self, move):
-        move.do_with(self)
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "Player(name=%s)" % repr(self.name)

50c7e8bd20d3053716c9bbfdc11d161eac17eaa4
Antoine Sachet <antoine.sac@gmail.com>
2017-09-25 23:34:22 +0100

    Main app now creates a TextController and a TextView. Also added basic logging, to be continued.

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
-import sys
-sys.path.append("..")
-
-from thekingsescape.game import Game
-from thekingsescape.player import Player
+from thekingsescape.controller import TextController
 from thekingsescape.view import TextView
-from thekingsescape.movefactory import MoveFactory
+from sys import stdout
+import logging
 
 
 def main():
-    p1 = Player("Antoine")
-    p2 = Player("Johanna")
+    logger = logging.Logger("thekingsescape",
+                            level=logging.DEBUG)
+    handler = logging.StreamHandler(stdout)
+    handler.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
 
-    g = Game(attackingPlayer=p1, defendingPlayer=p2)
-    v = TextView(g)
+    controller = TextController(stdout)
+    v = TextView(stdout)
+    controller.registerView(v)
 
-    v.render()
-
-    mf = MoveFactory(g)
-    m = mf.buildMoveFromString("A4 to E4", p1)
-
-    p1.perform(m)
-
-    v.render()
+    controller.start()
 
 
 if(__name__ == "__main__"):
